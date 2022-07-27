b82f7fcbb449a4a0acffc7f2cc9bcebc18e25766
TungPhung <phunglamtung2003@gmail.com>
2022-07-27 09:28:21 +0700

    commit

--- churnPrediction.py
+++ churnPrediction.py
@@ -50,10 +50,14 @@ smote = SMOTE(random_state = 14)
 
 feature_train, label_train= smote.fit_resample(feature_train, label_train)
 
+unique, counts = np.unique(label_train, return_counts=True)
+print(dict(zip(unique, counts)))
+
 
 label_train = np.asarray(label_train).astype('float32')
 
 label_test = np.asarray(label_test).astype('float32')
+
 
 #Function to design learning model:
 def model_design(feature, my_metrics, my_lr):
@@ -65,7 +69,6 @@ def model_design(feature, my_metrics, my_lr):
     model.add(layers.Dropout(0.2))
     model.add(layers.Dense(32, activation='relu'))
     model.add(layers.Dropout(0.1))
-    model.add(layers.Flatten())
     model.add(layers.Dense(1,activation='sigmoid'))
 
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(), metrics=my_metrics, optimizer=Adam(learning_rate=my_lr))
@@ -94,8 +97,8 @@ model=model_design(feature_train,METRICS, learning_rate)
 history=model.fit(feature_train,label_train,epochs=EPOCHS,batch_size=BATCH_SIZE,validation_split=VALIDATION_SPLIT,verbose=1, callbacks=[es])
 
 
-val_loss=model.evaluate(feature_test,label_test)
-print("loss: ", val_loss)
+metric_val=model.evaluate(feature_test,label_test)
+print("metrics_val: ", metric_val)
 
 
 #Calculate F1_score
